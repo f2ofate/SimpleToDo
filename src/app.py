a1160b7a34626a31fd0d335911a964647f10b46a
foofate <gungerfree@gmail.com>
2024-04-12 16:00:23 +0300

    init commit

---
+++ src/app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/todo_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean, default=False)
+
+@app.route("/")
+def home():
+    try:
+        todo_list = Todo.query.all()
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+    return render_template("main.html", todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    try:
+        title = request.form.get("title")
+        new_todo = Todo(title=title)
+        db.session.add(new_todo)
+        db.session.commit()
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+    return redirect(url_for("home"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    try:
+        todo = Todo.query.get_or_404(todo_id)
+        todo.complete = not todo.complete
+        db.session.commit()
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+    return redirect(url_for("home"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    try:
+        todo = Todo.query.get_or_404(todo_id)
+        db.session.delete(todo)
+        db.session.commit()
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+    return redirect(url_for("home"))
+
+@app.route("/delete_completed", methods=["GET", "POST"])
+def delete_completed():
+    try:
+        completed_todos = Todo.query.filter_by(complete=True).all()
+        for todo in completed_todos:
+            db.session.delete(todo)
+        db.session.commit()
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+    return redirect(url_for("home"))
+
+@app.route("/delete_all", methods=["GET", "POST"])
+def delete_all():
+    try:
+        all_todos = Todo.query.all()
+        for todo in all_todos:
+            db.session.delete(todo)
+        db.session.commit()
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
